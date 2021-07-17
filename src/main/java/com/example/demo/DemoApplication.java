9b7f0caf0558fbe249ab7fb7df35b4f68541ce28
midrob <midrob@ttu.ee>
2021-07-17 20:33:51 +0300

    fix encryption

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.awt.*;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class DemoApplication {
 
 	public static void main(String[] args) {
